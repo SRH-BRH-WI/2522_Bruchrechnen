2571d0daae8529a7ed146e1b474cdfa1b6c57782
Dr. Dillinger <peter.dillinger@srh.de>
2023-03-14 16:10:01 +0100

    Attribute, erster Konstruktor und erste Methode implementiert

--- src/Bruch.java
+++ src/Bruch.java
@@ -1,2 +1,13 @@
 public class Bruch {
+    private final long zähler;
+    private final long nenner;
+
+    public Bruch(long zähler, long nenner) {
+        this.zähler = zähler;
+        this.nenner = nenner;
+    }
+
+    public Bruch kehrWert() {
+        return new Bruch(nenner, zähler);
+    }
 }
