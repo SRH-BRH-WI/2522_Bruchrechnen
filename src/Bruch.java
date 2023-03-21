c6d83cf807b959d39a10a710d4c0e35d58c95a52
Dr. Dillinger <peter.dillinger@srh.de>
2023-03-21 09:38:56 +0100

    Bruch soweit ergänzt, Main berechnet einige Brüche, Methode mit Switch-Case geschrieben.

--- src/Bruch.java
+++ src/Bruch.java
@@ -7,11 +7,54 @@ public class Bruch {
         this.nenner = nenner;
     }
 
+    public Bruch add(Bruch b) {
+        long neuerZähler = this.zähler * b.nenner + b.zähler * this.nenner;
+        long neuerNenner = this.nenner * b.nenner;
+        return new Bruch(neuerZähler, neuerNenner).kürze();
+    }
+
+    public Bruch sub(Bruch b) {
+        return this.add( b.negiere() ).kürze();
+    }
+
+    public Bruch mult(Bruch b) {
+        return new Bruch( this.zähler * b.zähler, this.nenner * b.nenner ).kürze();
+    }
+
+    public Bruch div(Bruch b) {
+        return this.mult( b.kehrWert() ).kürze();
+    }
+
+    public Bruch kürze() {
+        long teiler = ggT(zähler, nenner);
+        return new Bruch(zähler / teiler, nenner / teiler);
+    }
+
+    public Bruch negiere() {
+        return new Bruch(-zähler, nenner);
+    }
+
     public Bruch kehrWert() {
         return new Bruch(nenner, zähler);
     }
 
     public String toString() {
         return zähler + "/" + nenner;
+    }
+
+    private long ggT(long a, long b) {
+        // sanity check
+        if (a == 0) return b;
+        if (b == 0) return a;
+        // mit negativen Zahlen klappt auch nicht
+        a = Math.abs(a);    // oder: if (a < 0) a = -a;
+        b = Math.abs(b);    //       if (b < 0) b = -b;
+
+        // uralter Algorithmus von Euklid
+        while (a != b) {
+            if (a > b) a -= b;
+            else b -= a;
+        }
+        return a;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
 public class Main {
+
+    public static void berechne(Bruch a, Bruch b, char operation) {
+        System.out.print(a + " " + operation + " " + b + " = ");
+
+        // Original Switch-Case Anweisung (break nicht vergessen!!!)
+//        switch (operation) {
+//            case '&':
+//            case '+': System.out.println( a.add(b) );   break;
+//            case '-': System.out.println( a.sub(b) );   break;
+//            case '*': System.out.println( a.mult(b) );  break;
+//            case ':':
+//            case '/': System.out.println( a.div(b) );   break;
+//            default:
+//                System.out.println("unbekannte Operation");
+//        }
+
+        // Verbesserte (enhanced) Switch-Case Anweisung (mit Lambdas)
+        switch (operation) {
+            case '&', '+' -> System.out.println(a.add(b));
+            case '-'      -> System.out.println(a.sub(b));
+            case '*'      -> System.out.println(a.mult(b));
+            case ':', '/' -> System.out.println(a.div(b));
+            default       -> System.out.println("unbekannte Operation");
+        }
+
+/*
+        if (operation == '+')      System.out.println( a.add(b) );
+        else if (operation == '-') System.out.println( a.sub(b) );
+        else if (operation == '*') System.out.println( a.mult(b) );
+        else if (operation == '/') System.out.println( a.div(b) );
+        else System.out.println("unbekannte Operation");
+ */
+    }
+
     public static void main(String[] args) {
         Bruch b1 = new Bruch(1, 2);
-        Bruch b2 = b1.kehrWert();
-        System.out.println("b1: " + b1 + ", b2: " + b2);
+        Bruch b2 = new Bruch(2, 3);
+        Bruch b3 = new Bruch(3, 4);
+
+        berechne(b1, b2, '+');
+        berechne(b1, b3, '-');
+        berechne(b1, b2, '*');
+        berechne(b1, b3, '/');
     }
 }
